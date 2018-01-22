3888dcc6818fd9c5106475a93ced3ff230c70dc1
David Finol <davidmfinol@gmail.com>
2018-01-22 15:54:14 -0600

    Update CardSelector.cs

--- Assets/Scripts/DeckEditor/CardSelector.cs
+++ Assets/Scripts/DeckEditor/CardSelector.cs
@@ -25,19 +25,40 @@ public class CardSelector : MonoBehaviour
     
     public void MoveLeft()
     {
-        if (EventSystem.current.alreadySelecting)
+        if (EventSystem.current.alreadySelecting || results.layoutArea.childCount < 1)
             return;
 
-        GameObject goToSelect = null;
-        while (goToSelect == null) {
+        for (int i = results.layoutArea.childCount - 1; i >= 0; i--) {
+            if (results.layoutArea.GetChild(i).GetComponent<CardModel>() != CardInfoViewer.Instance.SelectedCardModel)
+                continue;
+            i--;
+            if (i < 0) {
+                results.MoveLeft();
+                i = results.layoutArea.childCount - 1;
+            }
+            EventSystem.current.SetSelectedGameObject(results.layoutArea.GetChild(i).gameObject);
+            return;
         }
-        EventSystem.current.SetSelectedGameObject(goToSelect);
+        EventSystem.current.SetSelectedGameObject(results.layoutArea.GetChild(results.layoutArea.childCount - 1).gameObject);
     }
     
     public void MoveRight()
     {
-        if (EventSystem.current.alreadySelecting)
+        if (EventSystem.current.alreadySelecting || results.layoutArea.childCount < 1)
             return;
+
+        for (int i = 0; i < results.layoutArea.childCount; i++) {
+            if (results.layoutArea.GetChild(i).GetComponent<CardModel>() != CardInfoViewer.Instance.SelectedCardModel)
+                continue;
+            i++;
+            if (i == results.layoutArea.childCount) {
+                results.MoveRight();
+                i = 0;
+            }
+            EventSystem.current.SetSelectedGameObject(results.layoutArea.GetChild(i).gameObject);
+            return;
+        }
+        EventSystem.current.SetSelectedGameObject(results.layoutArea.GetChild(0).gameObject);
     }
     
     void Update()
