1cd1792cebb84813731b6d3c070f27cfe2b787ea
David Finol <davidmfinol@gmail.com>
2018-01-22 13:48:58 -0600

    Create CardSelector.cs

---
+++ Assets/Scripts/DeckEditor/CardSelector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CardSelector : MonoBehaviour
+{
+    public void MoveDown()
+    {
+    }
+    
+    public void MoveUp()
+    {
+    }
+    
+    public void MoveLeft()
+    {
+    }
+    
+    public void MoveRight()
+    {
+    }
+}
