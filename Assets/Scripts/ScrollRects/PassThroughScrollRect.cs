fe14228385c5d00cfc308040ac67938fdebefbc2
David Finol <davidmfinol@gmail.com>
2018-01-23 15:32:52 -0600

    Update PassThroughScrollRect.cs

--- Assets/Scripts/ScrollRects/PassThroughScrollRect.cs
+++ Assets/Scripts/ScrollRects/PassThroughScrollRect.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PassThroughScrollRect : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PassThroughScrollRect : ScrollRect
+{
+    public ScrollRect parentScrollRect;
+    public bool routeToParent = false;
+ 
+    public override void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        parentScrollRect?.OnInitializePotentialDrag(eventData);
+        base.OnInitializePotentialDrag(eventData);
+    }
+ 
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!horizontal && Math.Abs(eventData.delta.x) > Math.Abs(eventData.delta.y))
+            routeToParent = true;
+        else if (!vertical && Math.Abs(eventData.delta.x) < Math.Abs(eventData.delta.y))
+            routeToParent = true;
+        else
+            routeToParent = false;
+ 
+        if (routeToParent)
+            parentScrollRect?.OnBeginDrag(eventData);
+        else
+            base.OnBeginDrag(eventData);
+    }
+ 
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (routeToParent)
+            parentScrollRect?.OnDrag(eventData);
+        else
+            base.OnDrag(eventData);
+    }
+ 
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        if (routeToParent)
+            parentScrollRect?.OnEndDrag(eventData);
+        else
+            base.OnEndDrag(eventData);
+        
+        routeToParent = false;
+    }
 }
