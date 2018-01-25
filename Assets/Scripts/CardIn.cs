a5417fdba2ffc79a244742d54d04a21a92486ccf
David Finol <davidmfinol@gmail.com>
2018-01-25 14:16:31 -0600

    Add files via upload

---
+++ Assets/Scripts/CardIn.cs
@@ -0,0 +1,17 @@
+public static class CardIn
+{
+    public const string CancelInput = "Cancel";
+    public const string ColumnInput = "Column";
+    public const string DeleteInput = "Delete";
+    public const string FilterInput = "Filter";
+    public const string FocusNameInput = "FocusName";
+    public const string FocusTextInput = "FocusText";
+    public const string HorizontalInput = "Horizontal";
+    public const string LoadInput = "Load";
+    public const string NewInput = "New";
+    public const string PageInput = "Page";
+    public const string SaveInput = "Save";
+    public const string SortInput = "Sort";
+    public const string SubmitInput = "Submit";
+    public const string VerticalInput = "Vertical";
+}
