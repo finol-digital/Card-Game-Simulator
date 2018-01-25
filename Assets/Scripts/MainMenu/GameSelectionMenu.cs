8857851c0bd0034d06d5af0d67886a0b66120cfd
David Finol <davidmfinol@gmail.com>
2018-01-25 11:40:30 -0600

    Update GameSelectionMenu.cs

--- Assets/Scripts/MainMenu/GameSelectionMenu.cs
+++ Assets/Scripts/MainMenu/GameSelectionMenu.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameSelectionMenu : SelectionPanel
 {
+    public const string SubmitInput = "Submit";
+    public const string LoadInput = "Load";
+    public const string SaveInput = "Save";
+    public const string NewInput = "New";
+    public const string FocusNameInput = "FocusName";
+    public const string VerticalInput = "Vertical";
+    public const string CancelInput = "Cancel";
     public const string GameLoadErrorMessage = "Failed to load game url! ";
 
     public RectTransform downloadPanel;
     public InputField urlInput;
     public Button cancelButton;
     public Button downloadButton;
+    
+    void Update()
+    {
+        if (downloadPanel.gameObject.activeSelf) {
+            if (Input.GetButtonUp(SubmitInput) && downloadButton.interactable)
+                StartDownload();
+            else if (Input.GetButtonUp(SaveInput) && urlInput.interactable)
+                Paste();
+            else if (Input.GetButtonUp(NewInput) && urlInput.interactable)
+                Clear();
+            else if (Input.GetButtonUp(FocusNameInput) && urlInput.interactable)
+                urlInput.ActivateInputField();
+            else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown(CancelInput))
+                HideDownloadPanel();
+        } else {
+            if (Input.GetButtonDown(SubmitInput) && EventSystem.current.currentSelectedGameObject == null)
+                Hide();
+            else if (Input.GetButtonUp(LoadInput))
+                ShowDownloadPanel();
+            else if (Input.GetButtonDown(VerticalInput) && EventSystem.current.currentSelectedGameObject == null)
+                EventSystem.SetSelectedGameObject(selectionContent.GetChild(0));
+            else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown(CancelInput))
+                Hide();
+        }
+    }
 
     public void Show()
     {
