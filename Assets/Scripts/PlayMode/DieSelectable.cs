10ebb1097687e2647905cf61b55749c4b68d5590
David Finol <davidmfinol@gmail.com>
2018-01-25 16:30:58 -0600

    Update DieSelectable.cs

--- Assets/Scripts/PlayMode/DieSelectable.cs
+++ Assets/Scripts/PlayMode/DieSelectable.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-// HACK: THIS CLASS EXISTS TO DEAL WITH WONKINESS WITH SELECTING DIE BUTTONS
 public class DieSelectable : MonoBehaviour, IPointerDownHandler
 {
 	public GameObject dieObject;
