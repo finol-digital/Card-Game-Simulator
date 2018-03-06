3d97355dfa32bb6bdc6725b2fa9f769294350cec
David Finol <davidmfinol@gmail.com>
2018-03-06 17:19:25 -0600

    Create CGSNetData.cs

---
+++ Assets/Scripts/Multiplayer/CGSNetData.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public struct NetDeck {
+    public GameObject owner;
+    public List<string> cardIds;
+}
+
+public struct NetScore {
+    public GameObject owner;
+    public int points;
+}
+
+public class CGSNetData : NetworkBehaviour
+{
+    public class SyncListNetDeck : SyncListStruct<NetDeck> {}
+    public class SyncListNetScore : SyncListStruct<NetScore> {}
+    
+    public SyncListNetDeck decks = new SyncListNetDeck();
+    public SyncListNetScore scoreboard = new SyncListNetScore();
+    
+    public void RegisterDeck(GameObject owner, List<string> cardIds)
+    {
+        CmdAddDeck(owner, cardIds);
+    }
+    
+    [Command]
+    CmdAddDeck(GameObject owner, List<string> cardIds)
+    {
+        decks.Add(new NetDeck(owner, cardIds));
+    }
+    
+    public void RegisterScore(GameObject owner, int points)
+    {
+        CmdAddScore(owner, points);
+    }
+    
+    [Command]
+    CmdAddScore(GameObject owner, int point)
+    {
+        scoreboard.Add(new NetScore(owner, points));
+    }
+}
