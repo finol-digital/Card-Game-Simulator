5fbd80fda9ef7279dc37acd85a284d02eaa47c31
David Finol <davidmfinol@gmail.com>
2018-06-23 02:14:45 -0500

    Fix mac android build

--- Assets/Scripts/Editor/BuildCGS.cs
+++ Assets/Scripts/Editor/BuildCGS.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,6 +22,10 @@ class BuildCGS
 
     static void Android()
     {
+#if UNITY_EDITOR_OSX
+EditorPrefs.SetString("AndroidSdkRoot", "/Applications/Android/sdk");
+EditorPrefs.SetString("AndroidNdkRoot", "/Applications/android-ndk-r13b");
+#endif
         PlayerSettings.Android.keystorePass = Environment.GetCommandLineArgs().Last();
         PlayerSettings.Android.keyaliasPass = Environment.GetCommandLineArgs().Last();
 
