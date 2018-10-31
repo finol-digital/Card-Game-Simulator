3450494a30bb1d8f9b48fa3ee21f0a7a61cee3cd
David Finol <davidmfinol@gmail.com>
2018-10-30 21:38:37 -0500

    Travis CI

---
+++ Scripts/build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+project="Card Game Simulator"
+
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildOSXUniversalPlayer "$(pwd)/builds/osx/$project.app" 
+  -quit
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
+
+echo 'Attempting to zip build'
+zip -r $(pwd)/builds/mac.zip $(pwd)/builds/osx/
