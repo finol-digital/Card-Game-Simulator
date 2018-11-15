14041d81841bed01a471830a5dd9d390ebb516ad
David Finol <davidmfinol@gmail.com>
2018-11-14 19:04:38 -0600

    Update travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,34 +5,34 @@ project="Card Game Simulator"
 
 echo "Activating Unity license"
 ${UNITY_PATH} \
-    -logFile "${TRAVIS_BUILD_DIR}/unity.activation.log" \
+    -logFile "${pwd}/unity.activation.log" \
     -username ${UNITY_USER} \
     -password ${UNITY_PWD} \
     -batchmode \
     -noUpm \
     -quit
 echo "Unity activation log"
-cat "${TRAVIS_BUILD_DIR}/unity.activation.log"
+cat "${pwd}/unity.activation.log"
 
 echo "Attempting to build $project for OS X"
 ${UNITY_PATH} \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile "$(TRAVIS_BUILD_DIR)/unity.build.osx.log" \
-  -projectPath "$(TRAVIS_BUILD_DIR)" \
-  -buildOSXUniversalPlayer "$(TRAVIS_BUILD_DIR)/builds/osx/$project.app" \
+  -logFile "$(pwd)/unity.build.osx.log" \
+  -projectPath "$(pwd)" \
+  -buildOSXUniversalPlayer "$(pwd)/builds/osx/$project.app" \
   -quit
 echo 'Logs from build'
-cat "$(TRAVIS_BUILD_DIR)/unity.build.osx.log"
+cat "$(pwd)/unity.build.osx.log"
 
 echo "Returning Unity license"
 ${UNITY_PATH} \
-    -logFile "${TRAVIS_BUILD_DIR}/unity.returnlicense.log" \
+    -logFile "${pwd}/unity.returnlicense.log" \
     -batchmode \
     -returnlicense \
     -quit
-cat "$(TRAVIS_BUILD_DIR)/unity.returnlicense.log"
+cat "$(pwd)/unity.returnlicense.log"
 
 echo 'Attempting to zip OS X build'
-zip -r "$(TRAVIS_BUILD_DIR)/builds/mac.zip" "$(TRAVIS_BUILD_DIR)/builds/osx/"
+zip -r "$(pwd)/builds/mac.zip" "$(pwd)/builds/osx/"
