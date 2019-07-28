10580d69462a27a77cd8443cca096f73786e9e86
David Finol <davidmfinol@gmail.com>
2019-07-28 18:57:24 -0500

    Remove unused

--- Assets/Scripts/CGS/CardGameManager.cs
+++ Assets/Scripts/CGS/CardGameManager.cs
@@ -8,7 +8,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
