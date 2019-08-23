3a5ec31145fd2a8e8f42f2d8b8524f0e8ffe5fd0
David Finol <davidmfinol@gmail.com>
2019-08-23 02:57:06 -0500

    Fix mac build

--- travis-build/sign-osx-build.sh
+++ travis-build/sign-osx-build.sh
@@ -4,12 +4,12 @@ echo "Setting up osx certificates..."
 sleep 10
 
 KEY_CHAIN=osx.keychain
-APPLICATION_CERTIFICATE_P12=application.p12
-INSTALLER_CERTIFICATE_P12=installer.p12
+MAC_APPLICATION_CERTIFICATE_P12=mac_application.p12
+MAC_INSTALLER_CERTIFICATE_P12=mac_installer.p12
 
 # Recreate the certificates from the secure environment variable
-echo $OSX_APPLICATION_CERTIFICATE | base64 --decode > $APPLICATION_CERTIFICATE_P12
-echo $OSX_INSTALLER_CERTIFICATE | base64 --decode > $INSTALLER_CERTIFICATE_P12
+echo $MAC_APPLICATION_CERTIFICATE | base64 --decode > $MAC_APPLICATION_CERTIFICATE_P12
+echo $MAC_INSTALLER_CERTIFICATE | base64 --decode > $MAC_INSTALLER_CERTIFICATE_P12
 
 #create a keychain
 security create-keychain -p travis $KEY_CHAIN
@@ -20,8 +20,8 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p travis $KEY_CHAIN
 
-security import $APPLICATION_CERTIFICATE_P12 -k $KEY_CHAIN -P $OSX_APPLICATION_PASSWORD -T /usr/bin/codesign;
-security import $INSTALLER_CERTIFICATE_P12 -k $KEY_CHAIN -P $OSX_INSTALLER_PASSWORD -T /usr/bin/codesign;
+security import $MAC_APPLICATION_CERTIFICATE_P12 -k $KEY_CHAIN -P $MAC_APPLICATION_PASSWORD -T /usr/bin/codesign;
+security import $MAC_INSTALLER_CERTIFICATE_P12 -k $KEY_CHAIN -P $MAC_INSTALLER_PASSWORD -T /usr/bin/codesign;
 
 security find-identity -v
 
@@ -35,8 +35,10 @@ echo "OSX certificate setup complete!"
 echo "Signing app..."
 sleep 10
 
-codesign -f --deep -s "Developer ID Application: Finol Digital LLC (49G524X5NY)" --entitlements "${TRAVIS_BUILD_DIR}/Assets/Editor/Card Game Simulator.entitlements" "${HOME}/unity_build_cache/OSX/Card Game Simulator.app"
-productbuild --component "${HOME}/unity_build_cache/OSX/Card Game Simulator.app" /Applications --sign "Developer ID Installer: Finol Digital LLC (49G524X5NY)" "Card Game Simulator.pkg"
+codesign -f --deep -s "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" --entitlements "${TRAVIS_BUILD_DIR}/Assets/Editor/Card Game Simulator.entitlements" "${HOME}/unity_build_cache/OSX/Card Game Simulator.app"
+productbuild --component "${HOME}/unity_build_cache/OSX/Card Game Simulator.app" /Applications --sign "3rd Party Mac Developer Installer: Finol Digital LLC (49G524X5NY)" "Card Game Simulator.pkg"
+STATUS_CODE=$?
 
 sleep 10
-echo "App signed!"
+echo "App signed! Finishing with code $STATUS_CODE"
+exit $STATUS_CODE
