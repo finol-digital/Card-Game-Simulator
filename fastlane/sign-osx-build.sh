1ee992e2c21538b661c28f86c807f0242ad40de3
David Finol <davidmfinol@gmail.com>
2020-03-29 12:05:26 -0500

    Change from BUILD_DIR to BUILDS_PATH

--- fastlane/sign-osx-build.sh
+++ fastlane/sign-osx-build.sh
@@ -36,16 +36,16 @@ echo "OSX certificate setup complete!"
 echo "Signing app..."
 sleep 10
 
-chmod -R a+xr "${BUILD_DIR}/StandaloneOSX/Card Game Simulator.app"
-codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" "${BUILD_DIR}/StandaloneOSX/CardGameSimulator.app/Contents/Plugins/libProcessStart.bundle"
-codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" "${BUILD_DIR}/StandaloneOSX/CardGameSimulator.app/Contents/Plugins/FileBrowser.bundle"
-codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" --entitlements "Assets/Editor/Card Game Simulator.entitlements" "${BUILD_DIR}/StandaloneOSX/Card Game Simulator.app"
+chmod -R a+xr "${BUILDS_PATH}/StandaloneOSX/Card Game Simulator.app"
+codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" "${BUILDS_PATH}/StandaloneOSX/CardGameSimulator.app/Contents/Plugins/libProcessStart.bundle"
+codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" "${BUILDS_PATH}/StandaloneOSX/CardGameSimulator.app/Contents/Plugins/FileBrowser.bundle"
+codesign --deep --force --verbose --sign "3rd Party Mac Developer Application: Finol Digital LLC (49G524X5NY)" --entitlements "Assets/Editor/Card Game Simulator.entitlements" "${BUILDS_PATH}/StandaloneOSX/Card Game Simulator.app"
 
 sleep 10
 echo "Packaging app..."
 sleep 10
 
-productbuild --component "${BUILD_DIR}/OSX/Card Game Simulator.app" /Applications --sign "3rd Party Mac Developer Installer: Finol Digital LLC (49G524X5NY)" "Card Game Simulator.pkg"
+productbuild --component "${BUILDS_PATH}/OSX/Card Game Simulator.app" /Applications --sign "3rd Party Mac Developer Installer: Finol Digital LLC (49G524X5NY)" "Card Game Simulator.pkg"
 STATUS_CODE=$?
 
 sleep 10
