d272b1f8b10229f8dd9991756c7ee2621491c097
David Finol <davidmfinol@gmail.com>
2020-03-31 14:33:52 -0500

    Edit build script

--- Assets/Editor/BuildCGS.cs
+++ Assets/Editor/BuildCGS.cs
@@ -8,47 +8,6 @@ class BuildCGS
 {
     private static string EOL = Environment.NewLine;
 
-    static void SetAndroidSdkNdk()
-    {
-#if UNITY_EDITOR_LINUX
-        EditorPrefs.SetString("AndroidSdkRoot", "/home/david/Android/Sdk");
-        EditorPrefs.SetString("AndroidNdkRoot", "/home/david/android-ndk-r16b");
-#elif UNITY_EDITOR_OSX
-        EditorPrefs.SetString("AndroidSdkRoot", "/Applications/Android/sdk");
-        EditorPrefs.SetString("AndroidNdkRoot", "/Applications/android-ndk-r16b");
-#elif UNITY_EDITOR_WIN
-        EditorPrefs.SetString("AndroidSdkRoot", "C:/Users/Public/android-sdk");
-        EditorPrefs.SetString("AndroidNdkRoot", "C:/Users/Public/android-ndk-r16b");
-#endif
-    }
-
-    [MenuItem("CGS/Build Android")]
-    static void Android()
-    {
-        SetAndroidSdkNdk();
-        string [] buildArgs = Environment.GetCommandLineArgs();
-        PlayerSettings.Android.keystorePass = buildArgs[buildArgs.Length - 1];
-        PlayerSettings.Android.keyaliasPass = buildArgs[buildArgs.Length - 1];
-
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
-        buildPlayerOptions.locationPathName = buildArgs[buildArgs.Length - 2];
-        buildPlayerOptions.target = BuildTarget.Android;
-        buildPlayerOptions.options = BuildOptions.None;
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
-    }
-
-    [MenuItem("CGS/Build iOS")]
-    static void iOS()
-    {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
-        buildPlayerOptions.locationPathName = Environment.GetCommandLineArgs().Last();
-        buildPlayerOptions.target = BuildTarget.iOS;
-        buildPlayerOptions.options = BuildOptions.AcceptExternalModificationsToPlayer;
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
-    }
-
     public static void BuildProject()
     {
       // Gather values from args
