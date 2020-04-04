70deee540cdc1e5aafbe7c36042091f8e7c73ab4
David Finol <davidmfinol@gmail.com>
2020-04-04 00:52:17 -0500

    Initial implementation of list server

--- Assets/Scripts/CGS/Play/Multiplayer/CGSNetListServer.cs
+++ Assets/Scripts/CGS/Play/Multiplayer/CGSNetListServer.cs
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
 using UnityEngine;
 
 namespace CGS.Play.Multiplayer
@@ -39,13 +44,86 @@ namespace CGS.Play.Multiplayer
         public void StartClient()
         {
             Stop();
-// TODO
+            StartCoroutine(TickClient());
+        }
+
+        IEnumerator TickClient()
+        {
+            while (true)
+            {
+                if (_clientToListenConnection.Connected)
+                {
+                    // receive latest game server info
+                    while (_clientToListenConnection.GetNextMessage(out Telepathy.Message message))
+                    {
+                        // connected?
+                        if (message.eventType == Telepathy.EventType.Connected)
+                            Debug.Log("[List Server] Client connected!");
+                        // data message?
+                        else if (message.eventType == Telepathy.EventType.Data)
+                            OnServerFound(ParseMessage(message.data));
+                        // disconnected?
+                        else if (message.eventType == Telepathy.EventType.Disconnected)
+                            Debug.Log("[List Server] Client disconnected.");
+                    }
+                }
+                else if (!_clientToListenConnection.Connecting)
+                {
+                    Debug.Log("[List Server] Client connecting...");
+                    _clientToListenConnection.Connect(ListServerIp, ClientToListenPort);
+                }
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+
+        ServerStatus ParseMessage(byte[] bytes)
+        {
+            // note: we don't use ReadString here because the list server
+            //       doesn't know C#'s '7-bit-length + utf8' encoding for strings
+            BinaryReader reader = new BinaryReader(new MemoryStream(bytes, false), Encoding.UTF8);
+            byte ipBytesLength = reader.ReadByte();
+            byte[] ipBytes = reader.ReadBytes(ipBytesLength);
+            string ip = new IPAddress(ipBytes).ToString();
+            ushort gameNameLength = reader.ReadUInt16();
+            string gameName = Encoding.UTF8.GetString(reader.ReadBytes(gameNameLength));
+            return new ServerStatus(ip, gameName);
         }
 
         public void StartGameServer()
         {
             Stop();
-// TODO
+            StartCoroutine(TickGameServer());
+        }
+
+        IEnumerator TickGameServer()
+        {
+            while (true)
+            {
+                if (_gameServerToListenConnection.Connected)
+                    SendStatus();
+                else if (!_gameServerToListenConnection.Connecting)
+                {
+                    Debug.Log("[List Server] GameServer connecting......");
+                    _gameServerToListenConnection.Connect(ListServerIp, GameServerToListenPort);
+                }
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+
+        void SendStatus()
+        {
+            BinaryWriter writer = new BinaryWriter(new MemoryStream());
+
+            byte[] titleBytes = Encoding.UTF8.GetBytes(CardGameManager.Current.Name);
+            writer.Write((ushort)titleBytes.Length);
+            writer.Write(titleBytes);
+            writer.Flush();
+
+            // list server only allows up to 128 bytes per message
+            if (writer.BaseStream.Position <= 128)
+                _gameServerToListenConnection.Send(((MemoryStream)writer.BaseStream).ToArray());
+            else
+                Debug.LogError("[List Server] List Server will reject messages longer than 128 bytes. Please use a shorter title.");
         }
 
         public void Stop()
@@ -54,6 +132,7 @@ namespace CGS.Play.Multiplayer
                 _clientToListenConnection.Disconnect();
             if (_gameServerToListenConnection.Connected)
                 _gameServerToListenConnection.Disconnect();
+            StopAllCoroutines();
         }
 
     }
