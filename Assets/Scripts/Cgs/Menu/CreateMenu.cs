b50b00bfd09d16f30828826111290eb316ea74de
David Finol <davidmfinol@gmail.com>
2020-05-03 19:57:43 -0500

    Refactor with Rider

--- Assets/Scripts/Cgs/Menu/CreateMenu.cs
+++ Assets/Scripts/Cgs/Menu/CreateMenu.cs
@@ -24,9 +24,8 @@ namespace Cgs.Menu
         public const string DownloadBannerImagePrompt = "Enter banner image url...";
         public const string DownloadCardBackImage = "Download Card Back Image";
         public const string DownloadCardBackImagePrompt = "Enter card back image url...";
-#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+        // ReSharper disable once UnusedMember.Global
         public const string ImportImage = "Import Image";
-#endif
         public const string ImportImageWarningMessage = "No image file selected for import!";
         public const string CreateWarningMessage = "A game with that name already exists!";
         public const string CreationWarningMessage = "Failed to create the custom card game! ";
