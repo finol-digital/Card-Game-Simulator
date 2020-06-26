16cf529b0cd109e5a3ecaf4b4e06c0607fd15914
David Finol <davidmfinol@gmail.com>
2020-06-25 19:12:54 -0500

    Upgrade to Unity 2019.4 LTS

--- Assets/Scripts/CardGameDef/Unity/UnityDeck.cs
+++ Assets/Scripts/CardGameDef/Unity/UnityDeck.cs
@@ -6,11 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using CardGameDef.Decks;/*
-using Didstopia.PDFSharp;
-using Didstopia.PDFSharp.Drawing;
-using Didstopia.PDFSharp.Pdf;
-using MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes;*/
+using CardGameDef.Decks;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace CardGameDef.Unity
 {
@@ -249,8 +248,7 @@ namespace CardGameDef.Unity
         {
             if (!Directory.Exists(PrintPdfDirectory))
                 Directory.CreateDirectory(PrintPdfDirectory);
-/*
-            ImageSource.ImageSourceImpl = new ImageSharpImageSource();
+
             var pdfDocument = new PdfDocument();
             pdfDocument.Info.Title = Name;
 
@@ -285,7 +283,7 @@ namespace CardGameDef.Unity
             pdfDocument.Save(PrintPdfFilePath);
             pdfDocument.Close();
             pdfDocument.Dispose();
-*/
+
             return new Uri(UnityExtensionMethods.FilePrefix + PrintPdfFilePath);
         }
     }
