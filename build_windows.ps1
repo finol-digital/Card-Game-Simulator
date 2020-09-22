4986e39fd7ba5e81607b8c76e41badb78d004a59
David Finol <davidmfinol@gmail.com>
2020-09-21 19:54:55 -0500

    Add windows build

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,19 +1,19 @@
 # Activate License
-Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','–quit','-logfile','activatelicense.log','-nographics','-username',$env:UnityUser,'-password',$env:UnityPassword,'-serial',$env:UnitySerial -NoNewWindow -Wait
+Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','-quit','-logfile','activatelicense.log','-nographics','-username',$env:UnityUser,'-password',$env:UnityPassword,'-serial',$env:UnitySerial -NoNewWindow -Wait
 Get-Content activatelicense.log
 
 # Build Standalone
-Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','–quit','-logfile','build-windows.log','-executeMethod','Cgs.Editor.BuildCgs.BuildWindows' -NoNewWindow -Wait
+Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','-quit','-logfile','build-windows.log','-executeMethod','Cgs.Editor.BuildCgs.BuildWindows' -NoNewWindow -Wait
 Get-Content build-windows.log
 
 # Build Standalone64
-Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','–quit','-logfile','build-windows64.log','-executeMethod','Cgs.Editor.BuildCgs.BuildWindows64' -NoNewWindow -Wait
+Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','-quit','-logfile','build-windows64.log','-executeMethod','Cgs.Editor.BuildCgs.BuildWindows64' -NoNewWindow -Wait
 Get-Content build-windows64.log
 
 # Build Uwp
-Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','–quit','-logfile','build-uwp.log','-executeMethod','Cgs.Editor.BuildCgs.BuildUwp' -NoNewWindow -Wait
+Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','-quit','-logfile','build-uwp.log','-executeMethod','Cgs.Editor.BuildCgs.BuildUwp' -NoNewWindow -Wait
 Get-Content build-uwp.log
 
 # Return License
-Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','–quit','-logfile','returnlicense.log','-returnlicense' -NoNewWindow -Wait
+Start-Process -FilePath $env:UnityPath -ArgumentList '-batchmode','-quit','-logfile','returnlicense.log','-returnlicense' -NoNewWindow -Wait
 Get-Content returnlicense.log
