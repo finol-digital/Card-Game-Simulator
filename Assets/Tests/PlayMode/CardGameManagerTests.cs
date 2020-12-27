c52a1ad9048a2578bb43220adb74cd8e0c1a314c
David Finol <davidmfinol@gmail.com>
2020-12-26 23:26:07 -0600

    Add unit tests

---
+++ Assets/Tests/PlayMode/CardGameManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using Cgs;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class CardGameManagerTests
+    {
+        [UnityTest]
+        public IEnumerator CardGameManagerStarts()
+        {
+            var manager = new GameObject();
+            manager.AddComponent<CardGameManager>();
+            yield return new WaitForSeconds(0.1f);
+            Assert.IsTrue(CardGameManager.Current.HasReadProperties);
+        }
+    }
+}
