54b1c1e24c75062b48e95e26c10c014eecadcc92
David Finol <davidmfinol@gmail.com>
2021-11-11 12:26:03 -0600

    Update release notes

--- fastlane/get-release-notes.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-echo "Release Notes: "
-export RELEASE_NOTES="$(cat fastlane/metadata/en-US/release_notes.txt)"
-RELEASE_NOTES="${RELEASE_NOTES//'%'/'%25'}"
-RELEASE_NOTES="${RELEASE_NOTES//$'\n'/'%0A'}"
-RELEASE_NOTES="${RELEASE_NOTES//$'\r'/'%0D'}"
-echo "$RELEASE_NOTES"
-echo "::set-output name=RELEASE_NOTES::$RELEASE_NOTES"
