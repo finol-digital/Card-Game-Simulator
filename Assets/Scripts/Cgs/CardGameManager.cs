e33cd8eee3b09a01c22a1ea736e4e801533e0fa6
David Finol <davidmfinol@gmail.com>
2021-12-31 18:31:13 -0600

    Try deep link

--- Assets/Scripts/Cgs/CardGameManager.cs
+++ Assets/Scripts/Cgs/CardGameManager.cs
@@ -303,6 +303,7 @@ namespace Cgs
             Debug.Log("Checking Deep Links...");
 #if UNITY_IOS
             Debug.Log("Should use Firebase Dynamic Links for iOS...");
+            Application.deepLinkActivated += OnDeepLinkActivated;
             FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
             {
                 var dependencyStatus = task.Result;
