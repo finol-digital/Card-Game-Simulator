f802d93108440f4bd27af30ca5097da5f0ac2e86
David Finol <davidmfinol@gmail.com>
2024-09-24 17:54:16 -0500

    GA

--- docs/games/grand_archive_gabrary_spoilers/scraper.py
+++ docs/games/grand_archive_gabrary_spoilers/scraper.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
 from bs4 import BeautifulSoup
 import re
 import json
@@ -12,23 +12,32 @@ url = 'https://gabrary.net/sets/Spoilers/All'
 
 # Set up the WebDriver (make sure to download the appropriate driver)
 #driver = webdriver.Firefox()
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 
-# Driver from https://github.com/jsoma/selenium-github-actions
-#chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-#chrome_options = Options()
-#options = [
-#    "--headless",
-#    "--disable-gpu",
-#    "--window-size=1920,1200",
-#    "--ignore-certificate-errors",
-#    "--disable-extensions",
-#    "--no-sandbox",
-#    "--disable-dev-shm-usage"
-#]
-#for option in options:
-#    chrome_options.add_argument(option)
-#driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+# https://github.com/MarketingPipeline/Python-Selenium-Action/blob/main/Selenium-Template.py
+display = Display(visible=0, size=(800, 800))  
+display.start()
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+for option in options:
+    chrome_options.add_argument(option)
+driver = webdriver.Chrome(options = chrome_options)
 
 # Open the target URL
 driver.get(url)
