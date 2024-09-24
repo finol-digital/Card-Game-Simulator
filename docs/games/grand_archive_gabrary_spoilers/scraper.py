1092b3fcf75e8aaecce49c4de5a751d219f0a622
David Finol <davidmfinol@gmail.com>
2024-09-24 17:34:29 -0500

    GA

--- docs/games/grand_archive_gabrary_spoilers/scraper.py
+++ docs/games/grand_archive_gabrary_spoilers/scraper.py
@@ -12,23 +12,23 @@ url = 'https://gabrary.net/sets/Spoilers/All'
 
 # Set up the WebDriver (make sure to download the appropriate driver)
 #driver = webdriver.Firefox()
-#driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
 # Driver from https://github.com/jsoma/selenium-github-actions
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+#chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+#chrome_options = Options()
+#options = [
+#    "--headless",
+#    "--disable-gpu",
+#    "--window-size=1920,1200",
+#    "--ignore-certificate-errors",
+#    "--disable-extensions",
+#    "--no-sandbox",
+#    "--disable-dev-shm-usage"
+#]
+#for option in options:
+#    chrome_options.add_argument(option)
+#driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 # Open the target URL
 driver.get(url)
