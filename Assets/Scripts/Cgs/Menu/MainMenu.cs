c6c7708e67449d3c426ed01f999e95e94bae7bed
David Finol <davidmfinol@gmail.com>
2026-05-20 00:47:32 -0500

    Fix issues

--- Assets/Scripts/Cgs/Menu/MainMenu.cs
+++ Assets/Scripts/Cgs/Menu/MainMenu.cs
@@ -17,10 +17,6 @@ namespace Cgs.Menu
 {
     public class MainMenu : MonoBehaviour, IDragHandler, IEndDragHandler
     {
-        private const float MinWidth = 1200f;
-        private const float FooterPortraitWidth = 260f;
-        private const float FooterLandscapeWidth = 500f;
-
         public static string WelcomeMessage => "Welcome to CGS!\n" + WelcomeMessageExt;
 
 #if UNITY_ANDROID || UNITY_IOS
@@ -58,7 +54,7 @@ namespace Cgs.Menu
         public Image offRightImage;
         public List<GameObject> selectableButtons;
 
-        public Button fullscreenButton;
+        [SerializeField] Button fullscreenButton;
         [SerializeField] GameObject quitButton;
 
 #if !CGS_SINGLEGAME
@@ -96,41 +92,8 @@ namespace Cgs.Menu
             copyrightText.text = string.IsNullOrWhiteSpace(copyright) ? FinolDigitalLlc : copyright;
         }
 
-
-        private void OnRectTransformDimensionsChange()
-        {
-            if (!gameObject.activeInHierarchy)
-                return;
-
-            ResizeFooter();
-        }
-
-        private void ResizeFooter()
-        {
-            var screenWidth = ((RectTransform)transform).rect.width;
-
-            var sizeDelta = copyrightText.rectTransform.sizeDelta;
-            sizeDelta = screenWidth < MinWidth
-                ? new Vector2(FooterPortraitWidth, sizeDelta.y)
-                : new Vector2(FooterLandscapeWidth, sizeDelta.y);
-            copyrightText.rectTransform.sizeDelta = sizeDelta;
-
-            sizeDelta = versionText.rectTransform.sizeDelta;
-            sizeDelta = screenWidth < MinWidth
-                ? new Vector2(FooterPortraitWidth, sizeDelta.y)
-                : new Vector2(FooterLandscapeWidth, sizeDelta.y);
-            versionText.rectTransform.sizeDelta = sizeDelta;
-
-            sizeDelta = currentGameNameText.rectTransform.sizeDelta;
-            sizeDelta = screenWidth < MinWidth
-                ? new Vector2(-2f * FooterPortraitWidth, sizeDelta.y)
-                : new Vector2(-2f * FooterLandscapeWidth, sizeDelta.y);
-            currentGameNameText.rectTransform.sizeDelta = sizeDelta;
-        }
-
         private void Start()
         {
-            ResizeFooter();
             versionText.text = TitleScreen.VersionMessage;
 
 #if UNITY_WEBGL
